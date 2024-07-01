0e6c956f3c5f03e393c3c677d4c1029d4aa91e97
Taylor C. Powell <taylorp661@gmail.com>
2024-07-01 04:40:57 -0400

    Default commit message

---
+++ main.py
@@ -0,0 +1,11 @@
+laon_amount = 10000 # dollars
+equal_monthly_installments = 48
+# First installment is paid one month after the loan is initially taken out
+nominal_annual_interest_rate = 0.12 # Compounded monthly
+monthly_payment = ?
+
+
+
+
+
+
