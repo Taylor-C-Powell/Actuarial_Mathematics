a1d5b287deae1d6c7dde78f9a7f448051217e484
Taylor C. Powell <taylorp661@gmail.com>
2024-07-23 15:38:52 -0400

    Update: Updated find_bond_price() function

--- Bonds.py
+++ Bonds.py
@@ -2,5 +2,7 @@ from Annuities import *
 from TVM_Functions import *
 
 
-def find_bond_price(F, C, r, i, n):
+def find_bond_price(F, r, n, i, C):
+    if r == i:
+        return F
     return (F * r * annuity_immediate_PV(i, n)) + (C * v(i, n))
