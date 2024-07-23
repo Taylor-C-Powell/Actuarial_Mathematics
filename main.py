8ae05c346c0c5ceba40052ab072f47d53e54b2f1
Taylor C. Powell <taylorp661@gmail.com>
2024-07-23 14:24:08 -0400

    Update

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-laon_amount = 10000 # dollars
-equal_monthly_installments = 48
-# First installment is paid one month after the loan is initially taken out
-nominal_annual_interest_rate = 0.12 # Compounded monthly
-monthly_payment = ?
+from Annuities import *
+
+
+def calculate_compound_interest(interest_rate, accumulation_periods):
+    return pow((1 + interest_rate), accumulation_periods)
 
 
 
 
 
+def calculate_num_of_level_payments(previous_value, interest_rate, amount):
+    # calculates how many level payments can be made until the fund is depleted
+    # also calculates the value of the final payment (which is smaller than the level payments)
+    count = 0
+    while previous_value > amount:
+        previous_value = (previous_value * (1 + interest_rate)) - amount
+        count += 1
+    print("count: ", count)
+    print("final payment: ", previous_value)
 
+
+def calculate_num_of_increasing_payments(previous_value, interest_rate, initial_payment, amount_of_increase):
+    # calculates how many payments can be made until the fund is depleted
+    # payments increase at a fixed amount each payment period
+    # also calculates the value of the final payment
+    count = 0
+    payment = initial_payment
+    while previous_value > payment:
+        previous_value = (previous_value * (1 + interest_rate)) - payment
+        payment += amount_of_increase
+        count += 1
+    print(previous_value)
+
+
+def calculate_v(i, n):
+    return pow((1/(1+i)), n)
+
+
+def find_bond_price(F, C, r, i, n):
+    return (F * r * calculate_annuity_immediate_PV(i, n)) + (C * calculate_v(i, n))
+
+
+x = calculate_annuity_due_PV(0.08, 8)
+print(x)
